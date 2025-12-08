ffdaa986f4ec40ecfbcb32e721b035878b72a751
Latamk41 <latakambar70@gmail.com>
2025-12-08 20:01:12 +0530

    Add files via upload

---
+++ sumofarrays.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    
+    int a[5],b[5],c[5],i;
+    printf("enter the first array  elements are:");
+    for(i=0;i<5;i++)
+    scanf("%d",&a[i]);
+    printf("enter the second array elements are:");
+    for(i=0;i<5;i++)
+    scanf("%d",&b[i]);
+    for(i=0;i<5;i++)
+    {
+        c[i]=a[i]+b[i];
+        printf("\n third array element at index %d is=%d,i,c[i]");
+    }
+    
+    
+}
