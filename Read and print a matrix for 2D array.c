2932295691ec53105defbacd953ee21a8c400df3
Latamk41 <latakambar70@gmail.com>
2026-02-11 11:08:29 +0530

    Add files via upload

---
+++ Read and print a matrix for 2D array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int a[2][2], i, j;
+
+    printf("Enter 4 elements of the matrix:\n");
+
+    // Reading matrix
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("\nMatrix is:\n");
+
+    // Printing matrix
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++) {
+            printf("%d\t", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
